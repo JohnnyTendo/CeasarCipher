0a593e0839ea5709300f640833489f8473fca6b1
JohnnyTendo <40372786+JohnnyTendo@users.noreply.github.com>
2019-11-25 11:12:09 +0100

    Update helper.cpp

--- helper.cpp
+++ helper.cpp
@@ -3,10 +3,21 @@
 
 void PrintArray(char* _input)
 {
-    unsigned int n = strlen(_input); 
+    unsigned int n = strlen(_input);
     for (int i = 0; i < n; i++)
     {
         printf("%c", _input[i]);
     }
     printf("\r\n");
+}
+
+int GenerateKey(char* _key)
+{
+  long key = 1;
+  unsigned int n = strlen(_key);
+  for (int i = 0; i < n; i++)
+  {
+      key += (int)_key[i];
+      printf("added %d to %d\r\n",_key[i], key);
+  }
 }
