941e8aedb1b6a4e3c9f4f60e76424588640f0693
JohnnyTendo <40372786+JohnnyTendo@users.noreply.github.com>
2019-11-25 11:11:46 +0100

    Update header.h

--- header.h
+++ header.h
@@ -6,6 +6,10 @@ char* DecipherCapitols(char* encrypted, int key);
 char* CipherLetters(char* plain, int key);
 char* DecipherLetters(char* encrypted, int key);
 
+//AllCipher
+char* CipherAll(char* plain, int key);
+char* DecipherAll(char* plain, int key);
+
 //FileCipher
 void CipherFile(int algorithm, char* fileName, int key);
 void DecipherFile(int algorithm, char* fileName, int key);
@@ -13,3 +17,4 @@ void DecipherFile(int algorithm, char* fileName, int key);
 //Helper
 void PrintArray(char* _input);
 void UserInterface(char* _input);
+int GenerateKey(char* _key);
