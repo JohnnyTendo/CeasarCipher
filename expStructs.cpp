e96ca00bf3e783812dea7e21b65dac5c9e02b81b
JohnnyTendo <40372786+JohnnyTendo@users.noreply.github.com>
2019-12-10 14:37:49 +0100

    Update expStructs.cpp

--- expStructs.cpp
+++ expStructs.cpp
@@ -1,5 +1,14 @@
 void initializeStructures() 
 {
+  //a struct is similar to an object in an object-oriented language like c#
+  //one can initialize and declare a new instance of a struct:
+  //struct dataSet dataSetName;
+  
+  //and set a value to the variables stored in that struct with the syntax:
+  //datasetName.key = 123;
+  
+  //or get data from a struct:
+  //int cipherIndex = dataSetName.cipherIndex;
   struct dataSet
   {
     int key;
