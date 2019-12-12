852a11e53eb21a80a778b0010be96730fdbaa181
JohnnyTendo <40372786+JohnnyTendo@users.noreply.github.com>
2019-12-12 07:48:37 +0100

    Update interfaceHelper.cpp

--- interfaceHelper.cpp
+++ interfaceHelper.cpp
@@ -1,5 +1,114 @@
 #include <stdio.h>
 
+void userInterface(char* _input)
+{
+    int cipherIdx;
+    char key[100];
+    char direction;
+    char kind;
+    printf("File or text (f/t): \r\n");
+    scanf(" %c", &kind);
+    printf("___________________\r\n");
+    if (kind == 'f')
+    {
+        selectCipher(cipherIdx);
+        if (cipherIdx == 1 || cipherIdx == 2 || cipherIdx == 3)
+        {
+            selectKey(key);
+            selectDirection(direction);
+            if (direction == 'c')
+            {
+                selectText(_input);
+                cipherFile(cipherIdx, _input, generateKey(key));
+            }
+            else if (direction == 'd')
+            {
+                selectText(_input);
+                decipherFile(cipherIdx, _input, generateKey(key));
+            }
+            else
+            {
+                return;
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+    else if (kind == 't')
+    {
+        selectCipher(cipherIdx);
+        if (cipherIdx == 1)
+        {
+            selectKey(key);
+            selectDirection(direction);
+            if (direction == 'c')
+            {
+                selectText(_input);
+                //ref header.h for refactor
+                printArray(cipherCapitols(_input, generateKey(key)));
+            }
+            else if (direction == 'd')
+            {
+                selectText(_input);
+                printArray(decipherCapitols(_input, generateKey(key)));
+            }
+            else
+            {
+                return;
+            }
+        }
+        else if (cipherIdx == 2)
+        {
+            selectKey(key);
+            selectDirection(direction);
+            if (direction == 'c')
+            {
+                selectText(_input);
+                printArray(cipherLetters(_input, generateKey(key)));
+            }
+            else if (direction == 'd')
+            {
+                selectText(_input);
+                printArray(decipherLetters(_input, generateKey(key)));
+            }
+            else
+            {
+                return;
+            }
+        }
+        else if (cipherIdx == 3)
+        {
+            selectKey(key);
+            selectDirection(direction);
+            if (direction == 'c')
+            {
+                selectText(_input);
+                printArray(cipherAll(_input, generateKey(key)));
+            }
+            else if (direction == 'd')
+            {
+                selectText(_input);
+                printArray(decipherAll(_input, generateKey(key)));
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        else
+        {
+            return;
+        }
+    }
+    else
+    {
+        printf("No valid output type.\r\n");
+    }
+}
+
 void selectCipher(char* _cipherIdx)
 {
     printf("Select Cipher: \r\n");
