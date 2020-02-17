73914ff24d903c1a9ca912e90da3aee749be3d61
JohnnyTendo <40372786+JohnnyTendo@users.noreply.github.com>
2020-02-17 08:44:36 +0100

    Update header.h

--- header.h
+++ header.h
@@ -1,26 +1,17 @@
-//CapitolCipher
 void cipherCapitols(char* plain, int key);
 void decipherCapitols(char* encrypted, int key);
-
-//LetterCipher
+void cipherAll(char* plain, int key);
+void decipherAll(char* plain, int key);
 void cipherLetters(char* plain, int key);
 void decipherLetters(char* encrypted, int key);
 
-//AllCipher
-void cipherAll(char* plain, int key);
-void* decipherAll(char* plain, int key);
-
-//FileCipher
 void cipherFile(int algorithm, char* fileName, int key);
 void decipherFile(int algorithm, char* fileName, int key);
 
-//Helper
 void printArray(char* _input);
-void userInterface(char* _input);
-int generateKey(char* _key);
+void userInterface();
 
-//InterfaceHelper
-void selectCipher(char* _cipherIdx);
-void selectKey(int _key);
-void selectDirection(char* _direction);
+void selectCipher(int &_cipherIdx);
+void selectKey(int &_key);
+void selectDirection(char &_direction);
 void selectText(char* _input);
