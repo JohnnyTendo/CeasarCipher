8664d965774492e34b37329b392d5df05716de78
JohnnyTendo <40372786+JohnnyTendo@users.noreply.github.com>
2020-02-17 08:48:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "header.h"
+#include <string.h>
+#include <iostream>
+#include <fstream>
+
 
 int main()
 {
-    char plain[100];
-    userInterface(plain);
-    return 0;
+  userInterface();
+  return 0;
+}
+
+void selectCipher(int &_cipherIdx)
+{
+  printf("Select Cipher: \r\n");
+  printf("1 - CAPITOL LETTERS\r\n");
+  printf("2 -   all letters  \r\n");
+  printf("3 -    all chars   \r\n");
+  printf("___________________\r\n");
+  printf("Insert Index: ");
+  scanf("%d", &_cipherIdx);
+  printf("___________________\r\n");
+}
+
+void selectKey(int &_key)
+{
+  printf("Insert _key: ");
+  scanf("%d", &_key);
+  printf("___________________\r\n");
+}
+
+void selectDirection(char &_direction)
+{
+  printf("Cipher or decipher (c/d): ");
+  scanf(" %c", &_direction);
+  printf("___________________\r\n");
+}
+
+void selectText(char* _input)
+{
+  printf("Input text or filename: ");
+  scanf("%s", _input);
 }
