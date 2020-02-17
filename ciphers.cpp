261ab6996ec8710d71170c92e49d6019eb9c4e6e
JohnnyTendo <40372786+JohnnyTendo@users.noreply.github.com>
2020-02-17 08:45:15 +0100

    Update ciphers.cpp

--- ciphers.cpp
+++ ciphers.cpp
@@ -1,48 +1,43 @@
-#include <iostream>
-#include <fstream>
-
-#include <stdio.h>
 #include "header.h"
 #include <string.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
 
 //Works
 void cipherCapitols(char* plain, int key)
 {
-    key %= 26;
-    //Besser:
-    /*
-    * unsigned int n = sizeof(plain) / sizeof(plain[0])
-    */
-    unsigned int n = strlen(plain);
-    for (int i = 0; i < n; i++)
+  key %= 26;
+  unsigned int n = strlen(plain);
+  for (int i = 0; i < n; i++)
+  {
+    if (64 < plain[i] && plain[i] < 91)
     {
-        if (64 < plain[i] && plain[i] < 91)
-        {
-            plain[i] += key;
-            if (plain[i] > 90)
-            {
-                plain[i] -= 26;
-            }
-        }
+      plain[i] += key;
+      if (plain[i] > 90)
+      {
+        plain[i] -= 26;
+      }
     }
+  }
 }
 
 //Works
 void decipherCapitols(char* encrypted, int key)
 {
-    key %= 26;
-    unsigned int n = strlen(encrypted);
-    for (int i = 0; i < n; i++)
+  key %= 26;
+  unsigned int n = strlen(encrypted);
+  for (int i = 0; i < n; i++)
+  {
+    if (64 < encrypted[i] && encrypted[i] < 91)
     {
-        if (64 < encrypted[i] && encrypted[i] < 91)
-        {
-            encrypted[i] -= key;
-            if (encrypted[i] < 65)
-            {
-                encrypted[i] += 26;
-            }
-        }
+      encrypted[i] -= key;
+      if (encrypted[i] < 65)
+      {
+         encrypted[i] += 26;
+      }
     }
+  }
 }
 
 //works
@@ -53,9 +48,9 @@ void cipherAll(char* plain, int key)
   {
     int _value = plain[i];
     if((_value + key)>255)
-        plain[i] += (key - 255);
+       plain[i] += (key - 255);
     else
-        plain[i] += key;
+       plain[i] += key;
   }
 }
 
@@ -63,144 +58,124 @@ void cipherAll(char* plain, int key)
 void decipherAll(char* plain, int key)
 {
   unsigned int n = strlen(plain);
-  for (int i = 0; i < n; i++)
+  for (unsigned int i = 0; i < n; i++)
   {
     long _value = plain[i];
     if((_value - key) < 0)
-        plain[i] -= (key + 255);
+      plain[i] -= (key + 255);
     else
-        plain[i] -= key;
+      plain[i] -= key;
   }
 }
 
 //works
 void cipherLetters(char* plain, int key)
 {
-    key %= 52;
-    unsigned int n = strlen(plain);
-    for (int i = 0; i < n; i++)
+  key %= 52;
+  unsigned int n = strlen(plain);
+  for (int i = 0; i < n; i++)
+  {
+    int _value = plain[i];
+    if ((64 < _value && _value < 91) || (96 < _value && _value < 123))
     {
-        int _value = plain[i];
-        if ((64 < _value && _value < 91) || (96 < _value && _value < 123))
-        {
-            _value += key;
-            if (90 < _value && _value < 97)
-                _value += 6;
-            if (122 < _value)
-                _value -= 59;
-            plain[i] = _value;
-        }
+      _value += key;
+      if (90 < _value && _value < 97)
+        _value += 6;
+      if (122 < _value)
+         _value -= 59;
+      plain[i] = _value;
     }
+  }
 }
 
 //works
 void decipherLetters(char* encrypted, int key)
 {
-    key %= 52;
-    unsigned int n = strlen(encrypted);
-    for (int i = 0; i < n; i++)
+  key %= 52;
+  unsigned int n = strlen(encrypted);
+  for (int i = 0; i < n; i++)
+  {
+    int _value = encrypted[i];
+    if ((64 < _value && _value < 91) || (96 < _value && _value < 123))
     {
-        int _value = encrypted[i];
-        if ((64 < _value && _value < 91) || (96 < _value && _value < 123))
-        {
-            _value -= key;
-            if ((90 < _value && _value < 97) || (96 < (_value - key) && (_value - key) < 97))
-                _value -= 6;
-            else if (_value < 65)
-                _value += 59;
-            encrypted[i] = _value;
-        }
+      _value -= key;
+      if ((90 < _value && _value < 97) || (96 < (_value - key) && (_value - key) < 97))
+         _value -= 6;
+      else if (_value < 65)
+         _value += 59;
+      encrypted[i] = _value;
     }
+  }
 }
 
-//use fprint() , fopen() etc to reference the struct FILE instead
-
-//works but deprecated in cause of c++ usage
+//works
 void cipherFile(int algorithm, char* fileName, int key)
 {
-    //filestream ist cpp nicht c -> weg in reinem c suchen (structs hatten wir aber noch nicht)
-    //ggf structs mit erklären
-    std::ifstream iFile;
-    std::ofstream oFile;
-    iFile.open(fileName, std::ios::in);
-    iFile.seekg (0,iFile.end);
-    long size = iFile.tellg();
-    iFile.seekg (0);
-    char* buffer = new char[size];
-    iFile.read(buffer, size);
-    iFile.close();
-    if (algorithm == 1)
-        cipherCapitols(buffer, key);
-    else if (algorithm == 2)
-        cipherLetters(buffer, key);
-    else if (algorithm == 3)
-        cipherAll(buffer, key);
-    oFile.open(fileName, std::ios::out);
-    oFile.write(buffer, size);
-    delete[] buffer;
-    oFile.close();
-    printf("Ciphered file %s successfully.\r\n", fileName);
+  FILE *fileRead;
+  long lSize;
+  char * buffer;
+  fileRead = fopen(fileName, "r");
+  if (fileRead == NULL)
+  {
+    printf("Error opening file %s", fileName);
+    exit(1);
+  }
+  fseek(fileRead, 0, SEEK_END);
+  lSize = ftell (fileRead);
+  rewind(fileRead);
+  buffer = (char*) malloc (sizeof(char)*lSize);
+  fread(buffer, 1, lSize, fileRead);
+  fclose(fileRead);
+  FILE *fileWrite;
+  fileWrite = fopen(fileName, "w");
+  if (fileWrite == NULL)
+  {
+    printf("Error opening file %s", fileName);
+    exit(1);
+  }
+  if (algorithm == 1)
+    cipherCapitols(buffer, key);
+  else if (algorithm == 2)
+    cipherLetters(buffer, key);
+  else if (algorithm == 3)
+    cipherAll(buffer, key);
+  fwrite(buffer, 1, lSize, fileWrite);
+  fclose(fileWrite);
+  printf("Ciphered file %s successfully.\r\n", fileName);
 }
 
-//works but deprecated in cause of c++ usage
+//works
 void decipherFile(int algorithm, char* fileName, int key)
 {
-    std::ifstream iFile;
-    std::ofstream oFile;
-    iFile.open(fileName, std::ios::in);
-    iFile.seekg (0,iFile.end);
-    long size = iFile.tellg();
-    iFile.seekg (0);
-    char* buffer = new char[size];
-    iFile.read(buffer, size);
-    iFile.close();
-    if (algorithm == 1)
-        decipherCapitols(buffer, key);
-    else if (algorithm == 2)
-        decipherLetters(buffer, key);
-    else if (algorithm == 3)
-        decipherAll(buffer, key);
-    oFile.open(fileName, std::ios::out);
-    oFile.write(buffer, size);
-    delete[] buffer;
-    oFile.close();
-    printf("Deciphered file %s successfully.\r\n", fileName);
-}
-
-void cryptFile(int _cipherIdx, char* _input, int _key)
-{
-    _input = sprintf("%s.txt",_input);
-    FILE* file = fopen(_input, "r+");
-    long size = ftell(file);
-    char* buffer = new char[size];
-    fread(buffer, size, 1, file);
-    if (algorithm == 1)
-        cipherCapitols(buffer, key);
-    else if (algorithm == 2)
-        cipherLetters(buffer, key);
-    else if (algorithm == 3)
-        cipherAll(buffer, key);
-    fprintf(file, buffer);
-    fclose(file);
-    delete[] buffer;
-    printf("Deciphered file %s successfully.\r\n", _input);
-}
-
-void decryptFile(int _cipherIdx, char* _input, int _key)
-{
-    _input = sprintf("%s.txt",_input);
-    FILE* file = fopen(_input, "r+");
-    long size = ftell(file);
-    char* buffer = new char[size];
-    fread(buffer, size, 1, file);
-    if (algorithm == 1)
-        decipherCapitols(buffer, key);
-    else if (algorithm == 2)
-        decipherLetters(buffer, key);
-    else if (algorithm == 3)
-        decipherAll(buffer, key);
-    fprintf(file, buffer);
-    fclose(file);
-    delete[] buffer;
-    printf("Deciphered file %s successfully.\r\n", _input);
+  FILE *fileRead;
+  long lSize;
+  char * buffer;
+  fileRead = fopen(fileName, "r");
+  if (fileRead == NULL)
+  {
+    printf("Error opening file %s", fileName);
+    exit(1);
+  }
+  fseek(fileRead, 0, SEEK_END);
+  lSize = ftell (fileRead);
+  rewind(fileRead);
+  buffer = (char*) malloc (sizeof(char)*lSize);
+  fread(buffer, 1, lSize, fileRead);
+  fclose(fileRead);
+  FILE *fileWrite;
+  fileWrite = fopen(fileName, "w");
+  if (fileWrite == NULL)
+  {
+    printf("Error opening file %s", fileName);
+    exit(1);
+  }
+  if (algorithm == 1)
+    decipherCapitols(buffer, key);
+  else if (algorithm == 2)
+    decipherLetters(buffer, key);
+  else if (algorithm == 3)
+    decipherAll(buffer, key);
+  fwrite(buffer, 1, lSize, fileWrite);
+  fclose(fileWrite);
+  printf("Ciphered file %s successfully.\r\n", fileName);
 }
