4854d7cc8f43d43711c6175bf26af993e6a08cb3
JohnnyTendo <40372786+JohnnyTendo@users.noreply.github.com>
2020-02-17 08:47:20 +0100

    Update helper.cpp

--- helper.cpp
+++ helper.cpp
@@ -1,39 +1,127 @@
+#include "header.h"
 #include <stdio.h>
 #include <string.h>
 
 void printArray(char* _input)
 {
-    unsigned int n = strlen(_input);
-    for (int i = 0; i < n; i++)
-    {
-        printf("%c", _input[i]);
-    }
-    printf("\r\n");
-}
-
-int generateKey(char* _key)
-{
-  long key = 1;
-  unsigned int n = strlen(_key);
+  unsigned int n = strlen(_input);
   for (int i = 0; i < n; i++)
   {
-      key += (int)_key[i];
-      printf("added %d to %d\r\n",_key[i], key);
+    printf("%c", _input[i]);
   }
-    return key;
+  printf("\r\n");
 }
 
-int fileAsKey(char* _filePath)
+void userInterface()
 {
-    int sum;
-    FILE* file = fopen( _filePath, "r" );
-    if (!file)
+    char _input[100];
+    int cipherIdx;
+    int key;
+    char direction;
+    char kind;
+    printf("File or text (f/t): \r\n");
+    scanf(" %c", &kind);
+    printf("___________________\r\n");
+    if (kind == 'f')
     {
-        return 0;
+      selectCipher(cipherIdx);
+      if (cipherIdx == 1 || cipherIdx == 2 || cipherIdx == 3)
+      {
+        selectKey(key);
+        selectDirection(direction);
+        if (direction == 'c')
+        {
+          selectText(_input);
+          cipherFile(cipherIdx, _input, key);
+        }
+        else if (direction == 'd')
+        {
+          selectText(_input);
+          decipherFile(cipherIdx, _input, key);
+        }
+        else
+        {
+          return;
+        }
+      }
+      else
+      {
+        return;
+      }
     }
-    for (int i = 0; i < file.length; i++)
+    else if (kind == 't')
     {
-        sum += file[i];
+      selectCipher(cipherIdx);
+      if (cipherIdx == 1)
+      {
+        selectKey(key);
+        selectDirection(direction);
+        if (direction == 'c')
+        {
+          selectText(_input);
+          cipherCapitols(_input, key);
+          printArray(_input);
+        }
+        else if (direction == 'd')
+        {
+          selectText(_input);
+          decipherCapitols(_input, key);
+          printArray(_input);
+        }
+        else
+        {
+          return;
+        }
+      }
+      else if (cipherIdx == 2)
+      {
+        selectKey(key);
+        selectDirection(direction);
+        if (direction == 'c')
+        {
+          selectText(_input);
+          cipherLetters(_input, key);
+          printArray(_input);
+        }
+        else if (direction == 'd')
+        {
+          selectText(_input);
+          decipherLetters(_input, key);
+          printArray(_input);
+        }
+        else
+        {
+          return;
+        }
+      }
+      else if (cipherIdx == 3)
+      {
+        selectKey(key);
+        selectDirection(direction);
+        if (direction == 'c')
+        {
+          selectText(_input);
+          cipherAll(_input, key);
+          printArray(_input);
+        }
+        else if (direction == 'd')
+        {
+          selectText(_input);
+          decipherAll(_input, key);
+          printArray(_input);
+        }
+        else
+        {
+          return;
+        }
+      }
+      else
+      {
+        return;
+      }
     }
-    return sum;
+    else
+    {
+      printf("No valid output type.\r\n");
+    }
 }
